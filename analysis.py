07078d7be637c4275007d19bf7351693c93a4fb5
Thomas Schwalen <thomasschwalen@gmail.com>
2019-05-12 18:15:06 -0500

    Beginning subsection analysis

--- analysis.py
+++ analysis.py
@@ -5,8 +5,8 @@ from collections import Counter
 source="data/ivtt/ZL_ivtff_1b.txt"
 
 
-def word_frequencies(voynich, top=15):
-	list_of_pages = voynich[1]
+def word_frequencies(pages, top=15):
+	list_of_pages = pages
 
 	# base word frequencies
 	word_counts = Counter()
@@ -20,8 +20,10 @@ def word_frequencies(voynich, top=15):
 	for x in frequencies:
 		print("{word}  :  {freq}".format(word=x[0], freq=x[1]))
 
-def character_frequencies(voynich, top=15):
-	list_of_pages = voynich[1]
+	return word_counts
+
+def character_frequencies(pages, top=15):
+	list_of_pages = pages
 
 	# base word frequencies
 	char_counts = Counter()
@@ -36,10 +38,44 @@ def character_frequencies(voynich, top=15):
 	for x in frequencies:
 		print("{char}  :  {freq}".format(char=x[0], freq=x[1]))
 
-def preliminary_analysis(voynich):
+	return char_counts
+
+def subsection_analysis(voynich):
+	"""
+	In the trasciption file, each page is marked by what sort of illustration appears. The codes are as follows:
+	A    Astronomical (excluding zodiac)
+	B    Biological
+	C    Cosmological
+	H    Herbal
+	P    Pharmaceutical
+	S    Marginal stars only
+	T    Text‐only page (no illustrations)
+	Z    Zodiac
 	
-	#word_frequencies(voynich)
-	character_frequencies(voynich)
+	(I'm going to ignore "Text Only because the illustrations is what I'm most interested in here")
+
+	The labels might be a little arbitrary (I haven't seen all of the MS illustrations) but in any case, separating 
+	them now allows me to do both fine grained and coarse grained (combining categories like Astronomy and Cosmology)
+	analysis should I choose to do so.
+
+	"""
+
+	list_of_pages = voynich[1]
+
+	subsection_pages = {"A" : [], "B": [], "C": [], "H": [], "P": [], "S": [], "Z": [] }
+
+	for page in list_of_pages:
+		
+
+
+
+def preliminary_analysis(voynich):
+
+
+
+	
+	#word_frequencies(voynich[1])
+	#character_frequencies(voynich[1])
 
 
 
