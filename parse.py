5879710942e19fc9f51a885e6b17c5f4f2719192
Thomas Schwalen <thomasschwalen@gmail.com>
2019-05-11 22:03:20 -0500

    Amended regex for detecting page headers.

--- parse.py
+++ parse.py
@@ -1,12 +1,32 @@
 import re
+import pprint as pp
 
 
 source = "data/ivtt/ZL_ivtff_1b.txt"
 
+
+class VoynichPage:
+
+
+	def __init__(self, pg_name, pg_vars):
+		self.name = pg_name
+		self.vars = pg_vars
+
+		self.lines = []
+
+
+	def __str__(self):
+		return "<{n}>".format(n=self.name)
+
+	def __repr__(self):
+		return self.__str__()
+
 def read_in_source(source):
 
-
-	pg_header_re = "<f[0-9]+[rv]>\\s+<!.*>"
+	# < page . num , code ; T > 
+	# <f112v.11,+P0>   
+	pg_header_re = r"(<f[0-9]+[rv][0-9]?>)\s+(<!.*>)"
+	locus_re = r"<(f[0-9]+[rv])\.([0-9]+),(...)>"
 
 	with open(source, "r") as f:
 		
@@ -14,10 +34,10 @@ def read_in_source(source):
 
 		# read the header to know what format we're using
 		header = f.readline().split()
-		alphabet_string = header[1]
+		alphabet = header[1]
 
-		# print(header)
-		# print(alphabet_string)
+		current_page = None
+
 
 		# begin reading the rest of the file
 		line = f.readline().rstrip("\r\n")
@@ -28,16 +48,53 @@ def read_in_source(source):
 			if line[0] != "#":
 
 				# check if line is a page header, and read it if so
-				if re.match(pg_header_re, line):
-					print(line)
+				match = re.match(pg_header_re, line)
+				if match:
+					# print(line)
+					# print(match.groups())
+					pg_name = match.groups()[0][1:-1]
+					pg_vars = read_pg_vars(match.groups()[1])
+					#print(line)
+					
+					#print(pg_name)
+
+					new_page = VoynichPage(pg_name, pg_vars)
+
+					if current_page != None:
+						pages.append(new_page)
+					current_page = new_page
+				else:
+					pass
+					# if it's not a comment or page header, then it must be a regular line
+
+					# first get the locus
+					#locus = re.match(locus_re, line).groups()
 
 
 
+
+			# increment page
 			line = f.readline().rstrip("\r\n")
 
 
 
 
+
+		pp.pprint(pages)
+
+
+def read_pg_vars(vars_string):
+	# strip angle brackets and split into individual var assignments
+	pg_vars = vars_string[2 : -1].split()
+
+	pg_var_values = {}
+
+	# all variable names and values are 1 character and
+	# are in the format: $V=X
+	for stmt in pg_vars:
+		pg_var_values[stmt[1]] = stmt[3]
+
+	return pg_var_values
 
 					
 
